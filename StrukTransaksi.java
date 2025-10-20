ae85f5bf150d03ac76428331ca229a5545e646e0
imanmp-marriey <imanmardiyantipratama@gmail.com>
2025-10-20 22:53:21 +0700

    Menambahkan semua file class Aplikasi Sistem Kasir Toko Berbasis Console

--- StrukTransaksi.java
+++ StrukTransaksi.java
@@ -1,17 +1,25 @@
 public class StrukTransaksi {
-    public static void tampilkanStruk(Transaksi transaksi) {
-        System.out.println("=========== STRUK TRANSAKSI ===========");
+    private Transaksi transaksi;
+
+    // Constructor
+    public StrukTransaksi(Transaksi transaksi) {
+        this.transaksi = transaksi;
+    }
+
+    // Menampilkan struk transaksi
+    public void tampilkanStruk() {
+        System.out.println("======================================");
+        System.out.println("           STRUK TRANSAKSI            ");
+        System.out.println("======================================");
         System.out.println("ID Transaksi : " + transaksi.getIdTransaksi());
         System.out.println("Nama Pelanggan : " + transaksi.getPelanggan().getNamaPelanggan());
         System.out.println("--------------------------------------");
-        System.out.println("Daftar Barang:");
-        for (int i = 0; i < transaksi.getBarangList().size(); i++) {
-            Barang b = transaksi.getBarangList().get(i);
-            int jumlah = transaksi.getJumlahList().get(i);
-            System.out.println("- " + b.getNamaBarang() + " (" + jumlah + " x " + b.getHarga() + ") = " + (jumlah * b.getHarga()));
-        }
+        System.out.println("Daftar Barang : ");
+        System.out.println("- " + transaksi.getBarang().getNamaBarang() +
+                           " (" + transaksi.getJumlah() + " x " +
+                           transaksi.getBarang().getHarga() + ") = " + transaksi.getTotal());
         System.out.println("--------------------------------------");
-        System.out.println("Total Bayar : Rp " + transaksi.getTotalHarga());
+        System.out.println("Total Bayar : Rp " + transaksi.getTotal());
         System.out.println("Terima kasih telah berbelanja!");
         System.out.println("======================================");
     }
